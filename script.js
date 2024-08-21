3cfdd0b722e482122491e10b62cefee710a83850
Viet <viet18725@gmail.com>
2024-08-21 13:19:53 +0700

    Version1

---
+++ script.js
@@ -0,0 +1,23 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+
+}
+
+function clearDisplay() {
+    display.value = "";
+
+}
+
+function calculate() {
+    // display.value = eval(display.value);
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
